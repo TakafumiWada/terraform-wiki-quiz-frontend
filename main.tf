972be2d2923ca12a81e488194d0631101dde2b96
WadaTakafumi <wdtk2525@gmail.com>
2021-06-20 12:47:44 +0900

    terraformセットアップ

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("~/.config/gcloud/wiki-quiz-prod-credential.json")
+
+  project = "wiki-quiz-frontend-prod"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-c"
+}
+
+# resource "google_app_engine_application" "app" {
+#   project     = "wiki-quiz-frontend-prod"
+#   location_id = "asia-northeast1"
+# }
+
+
+resource "google_project_iam_member" "this" {
+  project = var.project_id
+  role    = "roles/appengine.appAdmin"
+  member  = "serviceAccount:${var.project_number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "service_account_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${var.project_number}@cloudbuild.gserviceaccount.com"
+}
+
+
+resource "google_cloudbuild_trigger" "this" {
+  project = "wiki-quiz-frontend-prod"
+  trigger_template {
+    branch_name = ".*"
+    repo_name   = "wiki-quiz-frontend"
+  }
+  filename = "cloudbuild.yaml"
+}
